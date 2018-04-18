d2b18336fd3dfa329991d87298a72ed1462dfd68
Chris Pollard <cpollard@cern.ch>
2018-04-18 11:31:08 +0200

    some additional histograms

--- plot.py
+++ plot.py
@@ -2,72 +2,128 @@ GeV = 1
 TeV = GeV*1e3
 MeV = GeV*1e-3
 
+# from
+# https://stackoverflow.com/questions/8793772/how-to-split-a-sequence-according-to-a-predicate
+def partition(pred, iterable):
+  from itertools import tee
+
+  def notpred(x):
+    return not pred(x)
+
+  'Use a predicate to partition entries into false entries and true entries'
+  # partition(is_odd, range(10)) --> 0 2 4 6 8   and  1 3 5 7 9
+  t1, t2 = tee(iterable)
+  return filter(pred, t1,), filter(notpred, t2)
+
+
+def hasAncestor(p, pid):
+  if p.id == pid:
+    return True
+  else:
+    for q in p.mothers():
+      if hasAncestor(q, pid):
+        return True
+      else:
+        continue
+
+  return False
+
 
 def main(infname, outfname):
-    import ROOT
-    import pylhe
+  import ROOT
+  import pylhe
+  import itertools as it
 
-    outf = ROOT.TFile(outfname, "CREATE")
+  outf = ROOT.TFile(outfname, "CREATE")
 
-    hmtt = ROOT.TH1F("hmtt", "hmtt", 100, 0, 2*TeV)
-    hptt1 = ROOT.TH1F("hptt1", "hptt1", 100, 0, 1*TeV)
-    hptt2 = ROOT.TH1F("hptt2", "hptt2", 100, 0, 1*TeV)
-    hptt3 = ROOT.TH1F("hptt3", "hptt3", 100, 0, 1*TeV)
-    hptt4 = ROOT.TH1F("hptt4", "hptt4", 100, 0, 1*TeV)
-    habsetat1 = ROOT.TH1F("habsetat1", "habsetat1", 100, 0, 5)
-    habsetat2 = ROOT.TH1F("habsetat2", "habsetat2", 100, 0, 5)
-    habsetat3 = ROOT.TH1F("habsetat3", "habsetat3", 100, 0, 5)
-    habsetat4 = ROOT.TH1F("habsetat4", "habsetat4", 100, 0, 5)
+  hptt1 = ROOT.TH1F("hptt1", "hptt1", 100, 0, 1*TeV)
+  hptt2 = ROOT.TH1F("hptt2", "hptt2", 100, 0, 1*TeV)
+  hptt3 = ROOT.TH1F("hptt3", "hptt3", 100, 0, 1*TeV)
+  hptt4 = ROOT.TH1F("hptt4", "hptt4", 100, 0, 1*TeV)
+  habsetat1 = ROOT.TH1F("habsetat1", "habsetat1", 100, 0, 5)
+  habsetat2 = ROOT.TH1F("habsetat2", "habsetat2", 100, 0, 5)
+  habsetat3 = ROOT.TH1F("habsetat3", "habsetat3", 100, 0, 5)
+  habsetat4 = ROOT.TH1F("habsetat4", "habsetat4", 100, 0, 5)
 
-    for evt in pylhe.readLHE(infname):
-        tops = \
-            [ ROOT.TLorentzVector(t.px, t.py, t.pz, t.e)
-              for t in evt.particles if abs(t.id) == 6
-            ]
+  hmtt_centraltops = ROOT.TH1F("hmtt_centraltops", "hmtt_centraltops", 100, 0, 2*TeV)
+  hmtt_leadingtops = ROOT.TH1F("hmtt_leadingtops", "hmtt_leadingtops", 100, 0, 2*TeV)
 
-        tops.sort(key = lambda t: t.Pt(), reverse=True)
+  hmtt_fromV = ROOT.TH1F("hmtt_fromV", "hmtt_fromV", 100, 0, 2*TeV)
 
-        if len(tops) != 4:
-            print "woops!"
-            continue
-        else:
-            hmtt.Fill((tops[0] + tops[1]).M(), evt.eventinfo.weight)
-            hptt1.Fill(tops[0].Pt(), evt.eventinfo.weight)
-            hptt2.Fill(tops[1].Pt(), evt.eventinfo.weight)
-            hptt3.Fill(tops[2].Pt(), evt.eventinfo.weight)
-            hptt4.Fill(tops[3].Pt(), evt.eventinfo.weight)
+  hpt_t1_fromV = ROOT.TH1F("hpt_t1_fromV", "hpt_t1_fromV", 100, 0, 1*TeV)
+  hpt_t2_fromV = ROOT.TH1F("hpt_t2_fromV", "hpt_t2_fromV", 100, 0, 1*TeV)
+  hpt_t1_notFromV = ROOT.TH1F("hpt_t1_notFromV", "hpt_t1_notFromV", 100, 0, 1*TeV)
+  hpt_t2_notFromV = ROOT.TH1F("hpt_t2_notFromV", "hpt_t2_notFromV", 100, 0, 1*TeV)
 
-            habsetat1.Fill(abs(tops[0].Eta()), evt.eventinfo.weight)
-            habsetat2.Fill(abs(tops[1].Eta()), evt.eventinfo.weight)
-            habsetat3.Fill(abs(tops[2].Eta()), evt.eventinfo.weight)
-            habsetat4.Fill(abs(tops[3].Eta()), evt.eventinfo.weight)
+  habseta_t1_fromV = ROOT.TH1F("habseta_t1_fromV", "habseta_t1_fromV", 100, 0, 5)
+  habseta_t2_fromV = ROOT.TH1F("habseta_t2_fromV", "habseta_t2_fromV", 100, 0, 5)
+  habseta_t1_notFromV = ROOT.TH1F("habseta_t1_notFromV", "habseta_t1_notFromV", 100, 0, 5)
+  habseta_t2_notFromV = ROOT.TH1F("habseta_t2_notFromV", "habseta_t2_notFromV", 100, 0, 5)
 
-    # scale by 100/fb
-    hmtt.Scale(100e3)
-    hptt1.Scale(100e3)
-    hptt2.Scale(100e3)
-    hptt3.Scale(100e3)
-    hptt4.Scale(100e3)
-    habsetat1.Scale(100e3)
-    habsetat2.Scale(100e3)
-    habsetat3.Scale(100e3)
-    habsetat4.Scale(100e3)
+  allHists = \
+    [ hptt1, hptt2, hptt3, hptt4, habsetat1, habsetat2
+    , habsetat3, habsetat4, hmtt_centraltops, hmtt_leadingtops
+    , hpt_t1_fromV, hpt_t2_fromV, hpt_t1_notFromV, hpt_t2_notFromV
+    , habseta_t1_fromV, habseta_t2_fromV, habseta_t1_notFromV
+    , habseta_t2_notFromV, hmtt_fromV 
+    ]
 
-    outf.Write()
-    outf.Close()
+  for evt in pylhe.readLHE(infname):
+    tops = [ t for t in evt.particles if abs(t.id) == 6 ]
 
-    return
+    # attach the TLV to the tops
+    for t in tops:
+      t.tlv = ROOT.TLorentzVector(t.px, t.py, t.pz, t.e)
 
+    tops.sort(key = lambda t: t.tlv.Pt(), reverse=True)
+    wgt = evt.eventinfo.weight
 
+    if len(tops) != 4:
+      print "woops!"
+      continue
 
-def fromV(p):
-    for m in p.mothers():
-        if m.id == 6000055:
-            return True
+    hptt1.Fill(tops[0].tlv.Pt(), wgt)
+    hptt2.Fill(tops[1].tlv.Pt(), wgt)
+    hptt3.Fill(tops[2].tlv.Pt(), wgt)
+    hptt4.Fill(tops[3].tlv.Pt(), wgt)
 
-    return False
+    habsetat1.Fill(abs(tops[0].tlv.Eta()), wgt)
+    habsetat2.Fill(abs(tops[1].tlv.Eta()), wgt)
+    habsetat3.Fill(abs(tops[2].tlv.Eta()), wgt)
+    habsetat4.Fill(abs(tops[3].tlv.Eta()), wgt)
+
+    hmtt_leadingtops.Fill((tops[0].tlv + tops[1].tlv).M(), wgt)
+
+    eta_ordered_tops = sorted(map(lambda t: (abs(t.tlv.Eta()), t), tops))
+    hmtt_centraltops.Fill(
+        (eta_ordered_tops[0][1].tlv + eta_ordered_tops[1][1].tlv).M(), wgt)
+
+    (topsFromV, topsNotFromV) = list(partition(lambda p: hasAncestor(p, 6000055), tops))
+
+    if len(topsFromV) == 2:
+      hpt_t1_fromV.Fill(topsFromV[0].tlv.Pt(), wgt)
+      hpt_t2_fromV.Fill(topsFromV[1].tlv.Pt(), wgt)
+      habseta_t1_fromV.Fill(abs(topsFromV[0].tlv.Eta()), wgt)
+      habseta_t2_fromV.Fill(abs(topsFromV[1].tlv.Eta()), wgt)
+      hmtt_fromV.Fill(
+          (topsFromV[0].tlv + topsFromV[1].tlv).M(), wgt)
+
+    if len(topsNotFromV) >= 2:
+      hpt_t1_notFromV.Fill(topsNotFromV[0].tlv.Pt(), wgt)
+      hpt_t2_notFromV.Fill(topsNotFromV[1].tlv.Pt(), wgt)
+      habseta_t1_notFromV.Fill(abs(topsNotFromV[0].tlv.Eta()), wgt)
+      habseta_t2_notFromV.Fill(abs(topsNotFromV[1].tlv.Eta()), wgt)
+
+  # scale by 100/fb
+  for h in allHists:
+    h.Scale(100e3)
+
+  outf.Write()
+  outf.Close()
+
+  return
 
 
 if __name__ == "__main__":
-    from sys import argv
-    main(argv[1], argv[2])
+  from sys import argv
+  main(argv[1], argv[2])
